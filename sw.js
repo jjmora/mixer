547c46387fcf4390f467bb3ecc01bfa7262a1f2a
jjmora <juanmora.dev@gmail.com>
2025-01-17 23:05:05 +0100

    urls update

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // sw.js
 const CACHE_NAME = 'audio-mixer-v1';
 const assetsToCache = [
-    './mixer/',
-    './mixer/index.html',
-    './mixer/manifest.json',
-    './mixer/icons/icon-192x192.png',
-    './mixer/icons/icon-512x512.png',
+    './',
+    './index.html',
+    './manifest.json',
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
